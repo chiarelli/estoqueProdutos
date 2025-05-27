3b34d8cba8c218946bc775d3a91f75d8264b072d
Raphael Mathias <oi.sucesso@gmail.com>
2025-05-27 01:13:01 -0300

    Dependência adicionada para transformar o nome do produto em slug

--- src/main/java/com/github/chiarelli/estoque_produtos/entities/Produtos.java
+++ src/main/java/com/github/chiarelli/estoque_produtos/entities/Produtos.java
@@ -1,23 +1,37 @@
 package com.github.chiarelli.estoque_produtos.entities;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 import java.util.UUID;
 
 import org.hibernate.validator.internal.util.stereotypes.Immutable;
+
+import com.github.slugify.Slugify;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Min;
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "tb_produtos")
-@AllArgsConstructor
+@Table(
+  name = "tb_produtos",
+  uniqueConstraints = {
+    @UniqueConstraint(name = "uk_produtos_nome_slug", columnNames = "nome_slug"),
+    @UniqueConstraint(name = "uk_produtos_nome", columnNames = "nome"),
+  }
+)
+@NoArgsConstructor
 @Getter
 public class Produtos {
 
@@ -26,7 +40,11 @@ public class Produtos {
   private UUID id;
 
   @Column(name = "nome", nullable = false, unique = true, length = 100)
+  @Size(min = 3, max = 100)
   private String nome;
+  
+  @Column(name = "nome_slug", nullable = false, unique = true)
+  private String nomeSlug;
   
   @Column(name = "preco", nullable = false)
   @Min(0)
@@ -39,5 +57,49 @@ public class Produtos {
   @ManyToOne
   @JoinColumn(name = "categoria_id", nullable = false)
   private Categorias categoria;
+
+  @Transient
+  final Slugify slg = Slugify.builder().build();
+
+  public Produtos(
+    UUID id, 
+    @Size(min = 3, max = 100) String nome, 
+    @Min(0) BigDecimal preco, 
+    @Min(0) Integer quantidade, 
+    Categorias categoria
+  ) {
+    this.id = id;
+    setNome(nome);
+    this.preco = preco;
+    this.quantidade = quantidade;
+    this.categoria = categoria;
+  }
+
+  public void setNome(@Size(min = 3, max = 100) String nome) {
+    this.nome = nome;
+    gerarSlug();
+  }
+
+  public void setPreco(@Min(0) BigDecimal preco) {
+    this.preco = preco;
+  }
+
+  public void setQuantidade(@Min(0) Integer quantidade) {
+    this.quantidade = quantidade;
+  }
+
+  public void setCategoria(Categorias categoria) {
+    if (Objects.nonNull(categoria)) {
+      this.categoria = categoria;
+    }
+  }
+
+  @PrePersist
+  @PreUpdate
+  public void gerarSlug() {
+    if (nome != null) {
+      this.nomeSlug = slg.slugify(nome).replace("-", "");
+    }
+  }
 
 }
